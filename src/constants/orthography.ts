2d3565efbc95354081d393c54dd6b0ac92897b40
ado-san <lopez.adrien@gmail.com>
2022-03-27 17:42:56 +0800

    Update orthography.ts

--- src/constants/orthography.ts
+++ src/constants/orthography.ts
@@ -30,16 +30,7 @@ export const ORTHOGRAPHY = [
 ]
 
 if (CONFIG.normalization) {
-  WORDS.forEach((val, i) => (WORDS[i] = val.normalize(CONFIG.normalization)))
-}
-
-function shuffle(array: any[]) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[array[i], array[j]] = [array[j], array[i]]
-  }
-}
-
-if (CONFIG.shuffle) {
-  shuffle(WORDS)
+  ORTHOGRAPHY.forEach(
+    (val, i) => (ORTHOGRAPHY[i] = val.normalize(CONFIG.normalization))
+  )
 }
